ea9c6077b37d321cc9a689c0e8e5e463673810a1
Ivan12341241 <vanooo123321@gmail.com>
2023-05-20 20:02:17 +0300

    fix_style
    git push origin style_updates

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
+import './assets/reset.css'
 
 import './assets/style.css'
 
